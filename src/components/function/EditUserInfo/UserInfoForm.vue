532f86ee0836eb6f0579b6377a5e689b6dcfafb4
ZRMYDYCG <547471919@qq.com>
2024-04-06 01:46:07 +0800

    🌙 add 基础组件 文本域 封装

--- src/components/function/EditUserInfo/UserInfoForm.vue
+++ src/components/function/EditUserInfo/UserInfoForm.vue
@@ -1,12 +1,13 @@
 <script setup>
 import YsInput from '@/components/base/ys-input/src/ys-input.vue'
+import YsTextfield from '@/components/base/ys-textfield/src/ys-textfield.vue'
 </script>
 
 <template>
   <span>昵称：</span>
   <ys-input></ys-input>
   <span>简介：</span>
-  <ys-textfield></ys-textfield>
+  <YsTextfield></YsTextfield>
 </template>
 
 <style scoped lang="scss"></style>
