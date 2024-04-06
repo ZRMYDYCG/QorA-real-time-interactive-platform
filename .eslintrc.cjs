454d757cbd96827ed5ca00d856ced817432227b4
ZRMYDYCG <547471919@qq.com>
2024-04-06 11:10:16 +0800

    🔆 废弃本套主题变脸切换方案

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,15 +7,15 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting',
-    // 解决 eslint、prettier 冲突问题
+    // 解决 eslint、prettier冲突问题
     'plugin:prettier/recommended',
     'eslint-config-prettier'
   ],
   rules: {
     //在rules中添加自定义规则
     // 添加组件命名忽略规则
-    "vue/multi-word-component-names": ["error",{
-      "ignores": ["index", "Layout"] //需要忽略的组件名
+    'vue/multi-word-component-names': ['error', {
+      'ignores': ['index', 'Layout'] //需要忽略的组件名
     }]
   },
   parserOptions: {
--- src/layout/components/LayoutHeader/theme-switch.vue
+++ src/layout/components/LayoutHeader/theme-switch.vue
@@ -1,10 +1,11 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
 const dark = ref(false)
 const themeChange = () => {
   const html = document.documentElement
   console.log(document.getElementById('theme-style'))
+  dark.value = !dark.value
   if (dark.value) {
     html.className = 'dark'
     // document.getElementById('theme-style').href = './src/styles/element/dark-theme.scss'
@@ -13,38 +14,24 @@ const themeChange = () => {
     // document.getElementById('theme-style').href = './src/styles/element/light-theme.scss'
   }
 }
+
+const IconType = computed(() => {
+  return dark.value ? 'Sunny' : 'MoonNight'
+})
 </script>
 
 <template>
-  <el-popover placement="top" title="主题切换" :width="150" trigger="hover">
-    <el-switch v-model="dark" size="small" @change="themeChange">
-      <template #active-action>
-        <span class="custom-active-action">
-          <el-icon><Sunny /></el-icon>
-        </span>
-      </template>
-      <template #inactive-action>
-        <span class="custom-inactive-action">
-          <el-icon><Moon /></el-icon>
-        </span>
-      </template>
-    </el-switch>
-    <template #reference>
-      <div class="theme-switch">
-        <el-button plain size="small" icon="Sunny" circle />
-      </div>
-    </template>
-  </el-popover>
+  <div class="theme-switch" @click="themeChange">
+    <el-button plain size="small" :icon="IconType" circle />
+  </div>
 </template>
 
 <style scoped lang="scss">
 .theme-switch {
   margin-right: 20px;
 
-  .el-switch {
-    --el-switch-on-color: #ffffff;
-    --el-switch-off-color: #000;
-    --el-switch-border-color: #ccc;
+  .el-button {
+    text-align: center;
   }
 }
 </style>
