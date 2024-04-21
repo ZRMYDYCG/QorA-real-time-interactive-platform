28ad6f236a4012f918a72b6801f22d50d02ba5ba
ZRMYDYCG <547471919@qq.com>
2024-04-21 15:04:41 +0800

    🔆 专栏开发

--- src/views/UserHome/children/userHomeColumn/index.vue
+++ src/views/UserHome/children/userHomeColumn/index.vue
@@ -1,9 +1,9 @@
-<script setup>
-// import ColumnItem from './components/column-item.vue'
-</script>
+<script setup></script>
 
 <template>
-  <div class="user-home-column"></div>
+  <div class="user-home-column">
+    <el-button size="small">新建专栏</el-button>
+  </div>
 </template>
 
 <style scoped lang="scss"></style>
